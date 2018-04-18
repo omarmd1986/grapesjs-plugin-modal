0f186282839cc244ddfcf5b2352fa52765e38057
Omar <omarmd1986@outlook.com>
2018-04-18 11:41:06 -0700

    Adding the block modal

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import grapesjs from 'grapesjs';
 import loadComponents from './components';
 import loadBlocks from './blocks';
 
-export default grapesjs.plugins.add('YOUR-PLUGIN-NAME', (editor, opts = {}) => {
-  const options = { ...{
-    // default options
-  },  ...opts };
+export default grapesjs.plugins.add('gjs-modal', (editor, opts = {}) => {
+    const options = {...{
+                category: `Avance`
+        }, ...opts};
 
   // Add components
   loadComponents(editor, options);
@@ -13,6 +13,4 @@ export default grapesjs.plugins.add('YOUR-PLUGIN-NAME', (editor, opts = {}) => {
   // Add blocks
   loadBlocks(editor, options);
 
-  // TODO Remove
-  editor.on('load', () => editor.addComponents(`<div style="margin:0 100px; padding:25px;">Content loaded from the plugin</div>`))
 });
