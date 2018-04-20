d5859897501741249909d970aaddb3abf13c3c2d
Omar <omarmd1986@outlook.com>
2018-04-20 15:36:57 -0700

    New Icon

--- src/blocks.js
+++ src/blocks.js
@@ -3,12 +3,11 @@ export default (editor, config = {}) => {
     const bm = editor.BlockManager;
 
     bm.add('modal', {
-
         label: 'Modal',
         content: `${config.modalStyle}<modal data-toggle="modal"></modal>`,
         category: config.category,
         attributes: {
-            class: 'gjs-fonts gjs-f-h1p'
+            class: 'fa fa-keyboard-o'
         }
     });
 }
